6602f229dce4e9c33795aba884f774f511dea604
Anwesh43 <anweshthecool0@gmail.com>
2021-05-09 15:09:24 +0530

    addng BiggerLineRotate class

--- index.ts
+++ index.ts
@@ -179,3 +179,26 @@ class BLRNode {
         return this 
     }
 }
+
+class BiggerLineRotate {
+
+    curr : BLRNode = new BLRNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
